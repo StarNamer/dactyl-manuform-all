71e5240f1284f80c229c81940222f5bb5ac79cdd
Rob Rogers <r2d2rogers@gmail.com>
2018-02-13 08:18:17 -0600

    WIP 5x7

--- create-models.sh
+++ create-models.sh
@@ -26,6 +26,16 @@ openscad -o things/right-5x6.stl things/right-5x6.scad >/dev/null 2>&1  &
 openscad -o things/left-5x6.stl  things/left-5x6.scad >/dev/null 2>&1 &
 git checkout src/dactyl_keyboard/dactyl.clj
 
+patch -p1 < 5x7.patch 
+lein run src/dactyl_keyboard/dactyl.clj
+cp things/right.scad things/right-5x7.scad
+cp things/left.scad things/left-5x7.scad
+cp things/right-plate.scad things/right-5x7-plate.scad
+openscad -o things/right-5x7-plate.dxf things/right-5x7-plate.scad >/dev/null 2>&1 &
+openscad -o things/right-5x7.stl things/right-5x7.scad >/dev/null 2>&1  &
+openscad -o things/left-5x7.stl  things/left-5x7.scad >/dev/null 2>&1 &
+git checkout src/dactyl_keyboard/dactyl.clj
+
 patch -p1 < 6x6.patch 
 lein run src/dactyl_keyboard/dactyl.clj
 cp things/right.scad things/right-6x6.scad
