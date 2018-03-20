141e7a1db37bb5a1fbceb511d08fc8a3c6375825
Toshiki Yagi <mrabbit@usagigoya.jp>
2018-03-20 17:25:02 +0900

    fix spacing

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -32,9 +32,9 @@
 
 (def thumb-offsets [6 -3 7])
 
-(def keyboard-z-offset 9)              ; controls overall height; original=9 with centercol=3; use 16 for centercol=2
+(def keyboard-z-offset 9)               ; controls overall height; original=9 with centercol=3; use 16 for centercol=2
 
-(def extra-width 2)                   ; extra space between the base of keys; original= 2
+(def extra-width 2)                     ; extra space between the base of keys; original= 2
 (def extra-height 1.0)                  ; original= 0.5
 
 (def wall-z-offset -15)                 ; length of the first downward-sloping part of the wall (negative)
