932cf9100dc4fa7fc3e38aee4f0012617190cf94
Yuya Yaguchi <yuya.yaguchi@gree.net>
2018-05-02 18:06:03 +0900

    hoge

--- create-models.sh
+++ create-models.sh
@@ -1,6 +1,6 @@
 lein run src/dactyl_keyboard/dactyl.clj
 cp things/right.scad things/right-5x7.scad
-#cp things/left.scad things/left-5x7.scad
+#cp things/left.scad things/left-5x6.scad
 openscad -o things/right-5x7.stl things/right-5x7.scad >/dev/null 2>&1 &
-#openscad -o things/left-5x7.stl  things/left-5x7.scad >/dev/null 2>&1 &
+#openscad -o things/left-5x6.stl  things/left-5x6.scad >/dev/null 2>&1 &
 wait
