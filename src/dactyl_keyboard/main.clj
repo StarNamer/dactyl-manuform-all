4e3abe4980cacbcd180f057b268bb2fefb54986c
zfhrp <coricozizi@gmail.com>
2018-09-25 14:57:20 +0900

    run main

--- src/dactyl_keyboard/main.clj
+++ src/dactyl_keyboard/main.clj
@@ -1,7 +1,5 @@
 (ns dactyl-keyboard.main)
 
 (defn -main []
-  ; (require 'dactyl-keyboard.dactyl)
-  ; (require 'dactyl-keyboard.lightcycle)
-  (require 'dactyl-keyboard.piste)
+  (require 'dactyl-keyboard.dactyl)
 )
