cb8fc371fcfbf97988e2457e6cf9278ff6ccd5c4
Seth Voltz <seth@designgods.net>
2019-08-13 20:37:27 -0700

    Improve project file

--- project.clj
+++ project.clj
@@ -4,6 +4,14 @@
   :main dactyl-keyboard.dactyl
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-watch "0.0.2"]]
+  :watch {
+    :rate 500 ;; check file every 500ms ('watchtower' is used internally)
+    :watchers {
+      :compile {
+        :watch-dirs ["src"]
+        :file-patterns [#"\.clj"]
+        :tasks ["run 1"]}}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [unicode-math "0.2.0"]
                  [scad-clj "0.4.0"]])
