9d0288a84a752f2b498cfdca2fc11ff5bd30046c
Seth Voltz <seth@designgods.net>
2019-08-15 22:14:02 -0700

    update build script and move patches to their own directory

--- create-models.sh
+++ create-models.sh
@@ -1,43 +1,51 @@
-lein run src/dactyl_keyboard/dactyl.clj
-cp things/right.scad things/right-4x5.scad
-cp things/left.scad things/left-4x5.scad
-cp things/right-plate.scad things/right-4x5-plate.scad
-openscad -o things/right-4x5-plate.dxf things/right-4x5-plate.scad >/dev/null 2>&1 &
-openscad -o things/right-4x5.stl things/right-4x5.scad >/dev/null 2>&1 &
-openscad -o things/left-4x5.stl  things/left-4x5.scad >/dev/null 2>&1 &
+#!/usr/bin/env bash -e
 
-patch -p1 < 4x6.patch 
-lein run src/dactyl_keyboard/dactyl.clj
-cp things/right.scad things/right-4x6.scad
-cp things/left.scad things/left-4x6.scad
-cp things/right-plate.scad things/right-4x6-plate.scad
-openscad -o things/right-4x6-plate.dxf things/right-4x6-plate.scad >/dev/null 2>&1 &
-openscad -o things/right-4x6.stl things/right-4x6.scad >/dev/null 2>&1  &
-openscad -o things/left-4x6.stl  things/left-4x6.scad >/dev/null 2>&1 &
-git checkout src/dactyl_keyboard/dactyl.clj
+PATCH_DIR=$(dirname "$0")/patches
+echo "Creating models and support files..."
 
-patch -p1 < 5x6.patch 
-lein run src/dactyl_keyboard/dactyl.clj
-cp things/right.scad things/right-5x6.scad
-cp things/left.scad things/left-5x6.scad
-cp things/right-plate.scad things/right-5x6-plate.scad
-openscad -o things/right-5x6-plate.dxf things/right-5x6-plate.scad >/dev/null 2>&1 &
-openscad -o things/right-5x6.stl things/right-5x6.scad >/dev/null 2>&1  &
-openscad -o things/left-5x6.stl  things/left-5x6.scad >/dev/null 2>&1 &
-git checkout src/dactyl_keyboard/dactyl.clj
+# Initial reset
+git checkout src/dactyl_keyboard/dactyl.clj >/dev/null 2>&1
 
-patch -p1 < 6x6.patch 
-lein run src/dactyl_keyboard/dactyl.clj
-cp things/right.scad things/right-6x6.scad
-cp things/left.scad things/left-6x6.scad
-cp things/right-plate.scad things/right-6x6-plate.scad
-openscad -o things/right-6x6-plate.dxf things/right-6x6-plate.scad >/dev/null 2>&1 &
-openscad -o things/right-6x6.stl things/right-6x6.scad >/dev/null 2>&1  &
-openscad -o things/left-6x6.stl  things/left-6x6.scad >/dev/null 2>&1 &
-git checkout src/dactyl_keyboard/dactyl.clj
+# Loop over each patch, generating scad, stl and plate dxf
+for file in `find ${PATCH_DIR} -name "*.patch" -type f | sort -V`; do
+  base=$(basename ${file} .patch)
+  echo "  Running $base"
 
+  printf "    - %-61s" "Patching base file..."
+  patch -p1 < ${file} >/dev/null
+  echo "done"
 
-# git add things/*-4x5.stl
-# git add things/right-4x5-plate.dxf
-# git commit -m "Add CAD files"
-wait
+  printf "    - %-61s" "Building model SCAD file..."
+  lein run src/dactyl_keyboard/dactyl.clj
+  echo "done"
+
+  printf "    - %-61s" "Copying files to correct names..."
+  cp things/right.scad things/right-${base}.scad
+  cp things/left.scad things/left-${base}.scad
+  cp things/right-plate.scad things/right-${base}-plate.scad
+  echo "done"
+
+  printf "    - %-61s" "Exporting DXF and STL files..."
+  openscad -o things/right-${base}-plate.dxf things/right-${base}-plate.scad >/dev/null 2>&1 &
+  openscad -o things/right-${base}.stl things/right-${base}.scad >/dev/null 2>&1  &
+  openscad -o things/left-${base}.stl  things/left-${base}.scad >/dev/null 2>&1 &
+  wait
+  echo "done"
+
+  printf "    - %-61s" "Resetting base file to default..."
+  git checkout src/dactyl_keyboard/dactyl.clj >/dev/null 2>&1
+  echo "done"
+
+  printf "    - %-61s" "Adding files to git..."
+  git add things/right-${base}-plate.dxf
+  git add things/right-${base}.stl
+  git add things/left-${base}.stl
+  echo "done"
+  echo
+done
+
+# Commit to git
+git commit -m "Update CAD files"
+echo
+
+echo "Done."
