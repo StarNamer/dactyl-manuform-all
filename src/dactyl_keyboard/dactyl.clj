e6d776eb5596cc1e6a7d9f56daa65af002eb1503
Maximiliano Korp <maxkorp@8bytealchemy.com>
2019-10-03 21:14:45 -0700

    modify keyholes for kailh
    taken from @rishka on the kailh-dactyl branch

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -76,18 +76,15 @@
                       (translate [0
                                   (+ (/ 1.5 2) (/ keyswitch-height 2))
                                   (/ plate-thickness 2)]))
-        left-wall (->> (cube 1.5 (+ keyswitch-height 3) plate-thickness)
+        left-wall (->> (cube 2 (+ keyswitch-height 3) plate-thickness)
                        (translate [(+ (/ 1.5 2) (/ keyswitch-width 2))
                                    0
                                    (/ plate-thickness 2)]))
-        side-nub (->> (binding [*fn* 30] (cylinder 1 2.75))
-                      (rotate (/ π 2) [1 0 0])
-                      (translate [(+ (/ keyswitch-width 2)) 0 1])
-                      (hull (->> (cube 1.5 2.75 plate-thickness)
-                                 (translate [(+ (/ 1.5 2) (/ keyswitch-width 2))
-                                             0
-                                             (/ plate-thickness 2)]))))
-        plate-half (union top-wall left-wall (with-fn 100 side-nub))]
+        kailh-cutout (->> (cube (/ keyswitch-width 3) 1.6 plate-thickness)
+                          (translate [0
+                                      (+ (/ 1.5 2) (+ (/ keyswitch-height 2)))
+                                      (/ plate-thickness)]))
+        plate-half (union (difference top-wall kailh-cutout) left-wall)]
     (union plate-half
            (->> plate-half
                 (mirror [1 0 0])
