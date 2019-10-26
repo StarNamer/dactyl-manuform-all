749c82181312a008c744202e28286792a5402f7a
LSChyi <alan81920@gmail.com>
2019-10-27 00:06:49 +0800

    discard nub

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -28,14 +28,7 @@
                        (translate [(+ (/ (/ keyborder 2) 2) (/ keyswitch-width 2))
                                    0
                                    (/ plate-thickness 2)]))
-        side-nub (->> (binding [*fn* 30] (cylinder 1 2.75))
-                      (rotate (/ π 2) [1 0 0])
-                      (translate [(+ (/ keyswitch-width 2)) 0 1])
-                      (hull (->> (cube (/ keyborder 2) 2.75 plate-thickness)
-                                 (translate [(+ (/ (/ keyborder 2) 2) (/ keyswitch-width 2))
-                                             0
-                                             (/ plate-thickness 2)]))))
-        plate-half (union top-wall left-wall (with-fn 100 side-nub))]
+        plate-half (union top-wall left-wall)]
     (union plate-half
            (->> plate-half
                 (mirror [1 0 0])
